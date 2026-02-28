d0c077abcd3a07916663adba9677e13f7e084036
jasperthefish <vrithikavaithi@gmail.com>
2026-02-28 10:39:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Write a Python program to copy odd lines from one file to another.
+# Write a Python program to copy odd lines from one file to another..
 
 file_r = open("Codingal.txt", "r")
 file_w = open("Codingal_Odd.txt", "w")
