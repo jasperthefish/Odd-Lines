527c708684c3b5f5ea69cce60eab66780d20ae7d
jasperthefish <vrithikavaithi@gmail.com>
2026-02-28 10:37:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 file_r = open("Codingal.txt", "r")
 file_w = open("Codingal_Odd.txt", "w")
 
+file_r_n = file_r.readlines()
 
-# for i in range()
-# for x in file_r:
-#     file_w.write(x)
+for i in range(1, len(file_r_n)+1, 2):
+    file_w.write(file_r_n[i-1])
